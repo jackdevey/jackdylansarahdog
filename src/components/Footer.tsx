7fa2156b8acc5afbe3911aa59d6217608be593e4
Jack Devey <jack@bandev.uk>
2022-02-17 20:37:39 +0000

    Compress images

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,11 @@ export default function Footer() {
         <div className="bg-gray-50">
             <footer className="footer max-w-7xl mx-auto py-3 sm:px-6 lg:px-8">
                 <h2 className="text-2xl mx-3 font-extrabold tracking-tight text-gray-900 mt-5">Jack, Dylan & Sarah Dog 🐶</h2>
-                <div className="container mx-3">
-                    <div className="mt-5 border-t border-gray-300 flex flex-col">
-                        <div className="text-left py-3">
-                            <p className="mt-3 text-sm text-gray-500">
-                                © {new Date().getFullYear()} <b>jack devey</b>, licensed under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL-3.0</a>
-                            </p>
-                        </div>
+                <div className="container mt-5 border-t border-gray-300 pl-3 pr-3">
+                    <div className="text-left py-3">
+                        <p className="mt-3 text-sm text-gray-500">
+                            © {new Date().getFullYear()} <b>jack devey</b>, licensed under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GPL-3.0</a>
+                        </p>
                     </div>
                 </div>
             </footer>
